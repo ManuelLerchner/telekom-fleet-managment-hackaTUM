555a67f03e441de55467b9fa136fd84df363cc48
“ge42gug” <ge42gug@mytum.de>
2024-11-23 22:27:03 +0100

    app changes

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ScenarioComponent } from './scenario/scenario.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
-  { path: '', component: ScenarioComponent },
+  { path: 'scenario', component: ScenarioComponent },
+  { path: '', component: LandingPageComponent },
 ];
 
 @NgModule({
